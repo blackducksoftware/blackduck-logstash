4f1922e2c18ef4bbbc06322d5dd90e5edfdd15ac
Joe O'Hara <1216155+Joe12o@users.noreply.github.com>
2019-03-06 16:05:13 -0500

    HUB-18751 Reduce log storage threshold to 14 days (#3)

--- deleteLogs.sh
+++ deleteLogs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 while true; do
-NUM_DAYS=${DAYS_TO_KEEP_LOGS:-30}
+NUM_DAYS=${DAYS_TO_KEEP_LOGS:-14}
 echo "Files deleted on $(date +%x_%r):" >> /var/log/deleteLogs.log
 find /var/lib/logstash/data/ -mindepth 2 -type f -mtime +$NUM_DAYS >> /var/log/deleteLogs.log
 find /var/lib/logstash/data/ -mindepth 2 -type f -mtime +$NUM_DAYS -delete
